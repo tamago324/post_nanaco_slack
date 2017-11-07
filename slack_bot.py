3fde773a3011e55b9d1bc3b6ac7f6edbbb435f91
tamago324 <tamago_pad@yahoo.co.jp>
2017-11-07 23:01:56 +0900

    add slack_bot.py
    slack_bot

---
+++ slack_bot.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import requests
+import json
+import os
+
+class SlackBot():
+    def __init__(self, username, iconUrl=None):
+        '''
+        Constractor
+        :param str username:    Bot name
+        :param str iconUrl: icon web url
+        ex1) slackBot = SlackBot('slackbot', iconUrl='https://example/dog.jpg')
+
+        -- free icon web site --
+        http://jp.freepik.com/free-icons/animals
+        '''
+        self._username = username
+        self._iconUrl = iconUrl
+
+    def post_nanaco_status(self, nanaco):
+        ''' Post message
+        :param str message:
+        '''
+        payload = self.create_payload(nanaco)
+        requests.post(os.environ['WEBHOOK_URL'], data=json.dumps(payload))
+
+    def create_payload(self, nanaco):
+        ''' Create payload for Nanaco object
+        :param Nanaco nanaco: nanaco object
+        :return: posted message payload
+        '''
+
+        payload = {'username': self._username,
+                   'icon_url': self._iconUrl}
+
+        if nanaco is None:
+            payload['text'] = 'nanacoの情報が正しく取得できませんでした'
+        else:
+            payload['text'] = f'{nanaco.money}/{nanaco.point}\n{nanaco.time}'
+
+        return payload
